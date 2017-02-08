8605bbceccf448a8ced3984a9a11bc82c3af9d96
QiyueSheng <dereksheng1226@gmail.com>
2017-02-07 20:30:07 -0500

    introduce socket.io into both Server side and Client side. test connection

--- Server/app.js
+++ Server/app.js
@@ -4,10 +4,17 @@ var express = require('express');
 
 var app = express();
 
+var http = require('http').Server(app);
+var socketio = require('socket.io')(http);
+
 var port = process.env.port || 3000;
 
 app.use(express.static(__dirname + "/../Client"));
 
-app.listen(port, function(){
+socketio.on('connection', function(socket){
+    console.log("A user connected.");
+});
+
+http.listen(port, function(){
     console.log("Running on PORT: " + port);
 });
