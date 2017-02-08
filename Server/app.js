1730da3567dfdf4414d1bc1e0e30473bfd5b98f7
QiyueSheng <dereksheng1226@gmail.com>
2017-02-07 22:05:29 -0500

    bring in vue and add both the server side and client side functionalities to chat

--- Server/app.js
+++ Server/app.js
@@ -10,9 +10,19 @@ var socketio = require('socket.io')(http);
 var port = process.env.port || 3000;
 
 app.use(express.static(__dirname + "/../Client"));
+app.use(express.static(__dirname + "/../node_modules"));
 
 socketio.on('connection', function(socket){
     console.log("A user connected.");
+
+    socket.on('message', function(message){
+        //console.log(message);
+        socketio.emit('message', message);
+    });
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
 });
 
 http.listen(port, function(){
