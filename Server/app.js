7cdd2a9b6181c63e485034e43ef03bd8f468ab5f
QiyueSheng <dereksheng1226@gmail.com>
2017-02-10 00:34:42 -0500

    change the layout more 'bootstrap' way and add the user login and username relative functionalities

--- Server/app.js
+++ Server/app.js
@@ -12,16 +12,44 @@ var port = process.env.port || 3000;
 app.use(express.static(__dirname + "/../Client"));
 app.use(express.static(__dirname + "/../node_modules"));
 
+var users = [];
+
 socketio.on('connection', function(socket){
     console.log("A user connected.");
 
+    socket.on('join', function(userName){
+        console.log('user change name to : ' + userName);
+
+        socket.userName = userName;
+        users.push(userName);
+
+        //notice it is not socket.emit('refreshUserList', users)
+        socketio.sockets.emit('refreshUserList', users);
+    });
+
     socket.on('message', function(message){
-        //console.log(message);
-        socketio.emit('message', message);
+        console.log(socket.userName + ' says: ' + message);
+
+        var data = {
+            userName: socket.userName,
+            message: message
+        };
+
+        socketio.emit('message', data);
     });
 
     socket.on('disconnect', function(){
-        console.log('user disconnected');
+
+        //when user log off, the name should be removed from the user list
+        var removedUserIndex = users.indexOf(socket.userName);
+        if(removedUserIndex >= 0){
+            users.splice(removedUserIndex, 1);
+        }
+
+        //notice it is not socket.emit('refreshUserList', users)
+        socketio.sockets.emit('refreshUserList', users);
+
+        console.log('user ' + socket.userName + ' disconnected');
     });
 });
 
